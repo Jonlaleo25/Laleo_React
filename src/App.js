eec5ab480d6ec050b2b390184591051373082860
Jonlaleo25 <jonathanar@librerialeo.com.mx>
2018-10-12 17:02:59 -0500

    parte3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Buscador from './componentes/Buscador';
+//import logo from './logo.svg';
 import './App.css';
 
+function Laleo(props){
+  return <h2>Laleo</h2>
+}
+
+function Laleo2(props){
+return <h2>{props.title}></h2>
+}
+
+
 class App extends Component {
+
+  state = {
+    termino : ""
+  }
+
+  consultarApi = ()=>{
+    const termino = this.state.termino;
+    const url =`https://pixabay.com/api/?key={ KEY }&q=${termino}`
+    //console.log(url);
+
+    fetch(url)
+    .then(respuesta => respuesta.json())
+    .then(resultado => console.log(resultado.hits))
+  }
+
+  datosBusqueda = (termino) =>{
+   this.setState({
+     termino
+   },()=>{
+     this.consultarApi();
+   })
+  }
+
   render() {
     return (
+      
       <div className="app container">
-        <div className = "jumbotron"></div>
+      <Laleo/>
+      <Laleo2 title='Laleo' />
+        <div className = "jumbotron">
+         <p className="lead text-center">Buscador </p>
+         <Buscador
+         datosBusqueda={this.datosBusqueda}/>
+        </div>
+        {this.state.termino}
       </div>
+      
       /*<div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
